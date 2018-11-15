ea09d790146701f96b2c0efe959325880aba58dd
ShaneN <shane94@hotmail.com>
2018-11-15 01:05:34 -0700

    add constructors

--- Xamarin.Forms.Platform.Android/ExportRendererAttribute.cs
+++ Xamarin.Forms.Platform.Android/ExportRendererAttribute.cs
@@ -5,7 +5,11 @@ namespace Xamarin.Forms
 	[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
 	public sealed class ExportRendererAttribute : HandlerAttribute
 	{
-		public ExportRendererAttribute(Type handler, Type target, Type[] supportedVisuals = null) : base(handler, target, supportedVisuals)
+		public ExportRendererAttribute(Type handler, Type target) : this(handler, target, null)
+		{
+		}
+
+		public ExportRendererAttribute(Type handler, Type target, Type[] supportedVisuals) : base(handler, target, supportedVisuals)
 		{
 		}
 	}
